e623c604eefa3cd7c7e301e77730d3ea2e58ee4b
hirenkiaku <VladislavAndreev1337@yandex.ru>
2024-09-13 10:57:24 +0500

    chore: added eslint config

---
+++ prod-course/eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "react/react-in-jsx-scope": "off",
+      "import/extensions": "off",
+    },
+    globals: {
+      __IS_DEV__: true,
+    },
+  },
+];
